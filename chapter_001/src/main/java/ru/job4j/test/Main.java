8441470c237e7aa6aa425905263e2cc74d06ae65
Mr. Anderson <h-blast@list.ru>
2020-07-08 09:09:32 +0200

    add Test

---
+++ chapter_001/src/main/java/ru/job4j/test/Main.java
@@ -0,0 +1,7 @@
+package ru.job4j.test;
+
+public class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
--- chapter_001/src/test/java/ru/job4j/Test.java
+++ chapter_001/src/test/java/ru/job4j/Test.java
@@ -1,11 +1,13 @@
 package ru.job4j;
 
 import org.junit.Assert;
+import ru.job4j.test.Main;
+
 import static org.hamcrest.core.Is.is;
 
 public class Test {
     @org.junit.Test
     public void test() {
-        Assert.assertThat(5, is(5));
+        Assert.assertThat(Main.sum(2, 3), is(5));
     }
 }
