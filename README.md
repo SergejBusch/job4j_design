198b0873d40f3b9257001b080f2e0ccfe2069e37
Mr. Anderson <h-blast@list.ru>
2020-07-13 17:01:41 +0200

    5.2.2. Implement Store<T extends Base> [#318423].

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![Build Status](https://travis-ci.org/SergejBusch/job4j_design.svg?branch=master)](https://travis-ci.org/SergejBusch/job4j_design)
-
 [![codecov](https://codecov.io/gh/SergejBusch/job4j_design/branch/master/graph/badge.svg)](https://codecov.io/gh/SergejBusch/job4j_design)
+
+job4j Junior
---
+++ chapter_001/src/main/java/ru/job4j/generic/MemStore.java
@@ -0,0 +1,49 @@
+package ru.job4j.generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MemStore<T extends Base> implements Store<T> {
+
+    private final List<T> mem = new ArrayList<>();
+
+    @Override
+    public void add(T model) {
+        mem.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        int index = indexOf(id);
+        if (index >= 0) {
+            mem.set(index, model);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        int index = indexOf(id);
+        if (index >= 0) {
+            mem.remove(index);
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public T findById(String id) {
+        return mem.get(indexOf(id));
+    }
+
+    public int indexOf(String id) {
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
