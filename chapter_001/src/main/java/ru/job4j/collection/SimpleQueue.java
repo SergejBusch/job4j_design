f20cd9f4322c2b205e30e93920c315f2478a3b33
Mr. Anderson <h-blast@list.ru>
2020-07-19 16:03:24 +0200

    5. Queue on two stacks [#318403]. Correction

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -7,25 +7,22 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-        isEmpty();
-        T result;
-        while (!in.isEmpty()) {
-            out.push(in.pop());
+        if (in.isEmpty()) {
+            throw new NoSuchElementException();
         }
-        result = out.pop();
-        while (!out.isEmpty()) {
-            in.push(out.pop());
-        }
+        T result = null;
+            if (out.isEmpty()) {
+                while (!in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+            if (!out.isEmpty()) {
+                result = out.pop();
+            }
         return result;
     }
 
     public void push(T value) {
         in.push(value);
-    }
-
-    private void isEmpty() {
-        if (in.isEmpty()) {
-            throw new NoSuchElementException();
-        }
     }
 }
