d991f3de93cde0c18e41962cb4b564342eb1d41c
Mr. Anderson <h-blast@list.ru>
2020-07-26 19:43:34 +0200

    1. Create elementary tree structure [#318418].

---
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,38 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> parentNode = findBy(parent);
+        Optional<Node<E>> childNode = findBy(child);
+        if (parentNode.isPresent() && childNode.isEmpty()) {
+            parentNode.get().children.add(new Node<>(child));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                result = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return result;
+    }
+}
