c6cd8200e51b23482e4830fc5d5d000793f35caa
Mr. Anderson <h-blast@list.ru>
2020-08-06 19:12:19 +0200

    3.0. Testing IO [#318447]

--- chapter_002/src/test/java/ru/job4j/io/AnaliseTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnaliseTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 import java.util.List;
@@ -12,9 +14,14 @@ public class AnaliseTest {
 
     private static int index = 0;
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenTwoTimeUnavailable() {
-        try (var out = new PrintWriter(new FileOutputStream("../source.txt"))) {
+    public void whenTwoTimeUnavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (var out = new PrintWriter(source)) {
             out.println("200 10:56:01");
             out.println("500 10:57:01");
             out.println("400 10:58:01");
@@ -24,9 +31,9 @@ public class AnaliseTest {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        new Analise().unavailable("../source.txt", "../target.txt");
+        new Analise().unavailable(source.getPath(), target.getPath());
         var resultData = List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;");
-        try (var in = new BufferedReader(new FileReader("../target.txt"))) {
+        try (var in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(n ->
                 Assert.assertThat(n, is(resultData.get(index++))));
         } catch (Exception ex) {
