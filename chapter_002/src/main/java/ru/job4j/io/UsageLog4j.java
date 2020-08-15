d24c34ba20304a540256ee27c15a0c3738c3f886
Mr. Anderson <h-blast@list.ru>
2020-08-15 23:31:52 +0200

    3. Slf4j - output variables. [#318443]

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,14 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int i = 33;
+        double d = 70.5;
+        char a = 'a';
+        float f = 5.5f;
+        short s = 8;
+        boolean b = s + f > i;
+        byte by = 9;
+        long l = 10;
+        LOG.debug(" {},  {},  {},  {},  {},  {},  {},  {}", i, d, a, f, s, b, by, l);
     }
 }
