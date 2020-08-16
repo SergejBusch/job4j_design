3ecd71e29b2330f21df51a1c3b29df727843a7e1
Mr. Anderson <h-blast@list.ru>
2020-08-16 12:02:01 +0200

    6. The coding. [#318458] Correction

--- chapter_002/src/main/java/ru/job4j/io/Chat.java
+++ chapter_002/src/main/java/ru/job4j/io/Chat.java
@@ -3,11 +3,18 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Chat {
+    private static final String PAUSE = "pause";
+    private static final String START = "start";
+    private static final String STOP = "stop";
+
     private Scanner scanner = new Scanner(System.in);
+    private List<String> text = new ArrayList<>();
     private String line;
     private Path file;
     private boolean start = true;
@@ -17,22 +24,27 @@ public class Chat {
         if (!file.toFile().isFile()) {
             throw new FileNotFoundException("Oh no");
         }
-    }
-
-    public void startChat() {
-        try (var out = new PrintWriter(new FileOutputStream("chatOutput.txt"))) {
-            while (!nextLine().equals("stop")) {
-                out.write(line + System.lineSeparator());
-                if (check(line)) {
-                    printRandomTextFromFile(out);
-                }
-            }
-            out.write(line + System.lineSeparator());
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (var in = new BufferedReader(new FileReader(file.toFile()))) {
+            in.lines().forEach(text::add);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-
     }
+
+    public void startChat() throws IOException {
+        var textList = new ArrayList<String>();
+        while (!nextLine().equals(STOP)) {
+            textList.add(line);
+            if (check(line)) {
+                printRandomTextFromFile(textList);
+            }
+        }
+        textList.add(line);
+        var writer = new FileWriter("textOutput.txt");
+        for (String str : textList) {
+            writer.write(str + System.lineSeparator());
+        }
+        writer.close();    }
 
     private String nextLine() {
         line = scanner.nextLine();
@@ -40,32 +52,21 @@ public class Chat {
     }
 
     private boolean check(String line) {
-        if (start && line.equals("pause")) {
+        if (start && line.equals(PAUSE)) {
             start = false;
-        } else if (!start && line.equals("start")) {
+        } else if (!start && line.equals(START)) {
             start = true;
         }
         return start;
     }
 
-    private void printRandomTextFromFile(PrintWriter out) {
-        try (var in = new BufferedReader(new FileReader(file.toFile()))) {
-            var sb = new StringBuilder();
-            in.lines().forEach(sb::append);
-            String[] stringArray = sb.toString().split(" ");
-            var position = new Random().nextInt(stringArray.length - 5);
-            for (int i = position; i < new Random().nextInt(3) + 3 + position; i++) {
-                System.out.print(stringArray[i] + " ");
-                out.write(stringArray[i] + " ");
-            }
-            System.out.println();
-            out.write(System.lineSeparator());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+    private void printRandomTextFromFile(List<String> out) {
+            var position = new Random().nextInt(text.size() - 1);
+            System.out.println(text.get(position));
+            out.add(text.get(position));
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         new Chat("text.txt").startChat();
     }
 }
