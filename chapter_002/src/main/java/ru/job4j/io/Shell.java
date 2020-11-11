7344e89350da161cb10f71f357654376f3f11b2d
Mr. Anderson <h-blast@list.ru>
2020-11-11 22:35:50 +0100

    Shell

--- chapter_002/src/main/java/ru/job4j/io/Shell.java
+++ chapter_002/src/main/java/ru/job4j/io/Shell.java
@@ -1,69 +1,46 @@
 package ru.job4j.io;
 
-import java.io.File;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Shell {
-    private final String root = "/";
-    private LinkedList<String> rootDirs = new LinkedList<>();
-    private File temp;
-    private ListIterator<String> listIterator;
+    String dir = "/";
+    Map<String, String> map;
 
     public Shell() {
-        rootDirs.addAll(List.of("root", "user", "local"));
-        temp = new File("root/user/local");
-        temp.mkdirs();
-        temp = new File("root");
-        listIterator = rootDirs.listIterator(1);
+        map = new LinkedHashMap<>();
+        map.put("/", "/");
+        map.put("user", "/user");
+        map.put("local", "/user/local");
     }
 
     public void cd(String path) {
-        if (path.charAt(0) == root.charAt(0)) {
-            if (path.length() > 1) {
-                String pathFromFirstIndex = path.substring(1);
-                if (pathFromFirstIndex.contains("/")) {
-                    String[] dirs = pathFromFirstIndex.split("/");
-                    listIterator = rootDirs.listIterator(1);
-                    for (String dir : dirs) {
-                        cd(dir);
-                    }
-                }
-            } else {
-                temp = new File("root");
-            }
-        } else {
-            if (path.equals("..")) {
-                if (listIterator.hasPrevious()) {
-                    temp = getPath(rootDirs.indexOf(listIterator.previous()));
-                }
-            } else if (listIterator.hasNext()) {
-                String dir;
-                if (path.equals(dir = listIterator.next())) {
-                    temp = getPath(rootDirs.indexOf(dir) + 1);
-                } else {
-                    listIterator.previous();
-                }
-            }
-        }
+       split(path);
+       if (path.equals("..")) {
+           positionDecrement();
+       } else {
+           dir = map.getOrDefault(path, dir);
+
+       }
     }
 
     public String pwd() {
-        String result = temp.getPath().replace("root/", root);
-        result = result.equals("root") ? root : result;
-        return result;
+        return dir;
     }
 
-    private File getPath(int index) {
-        if (index > 0) {
-            List<String> list = rootDirs.subList(0, index);
-            StringBuilder tempPath = new StringBuilder();
-            for (String temp : list) {
-                tempPath.append(temp).append("/");
+    private void split(String paths) {
+        if (paths.substring(1).contains("/")) {
+            for(String path : paths.substring(1).split("/")) {
+                cd(path);
             }
-            return new File(tempPath.toString());
         }
-        return temp;
     }
+
+    private void positionDecrement() {
+       var list = new ArrayList<>(map.values());
+       int index = list.indexOf(dir);
+       dir = index > 0 ? list.get(--index) : dir;
+
+    }
+
+
 }
