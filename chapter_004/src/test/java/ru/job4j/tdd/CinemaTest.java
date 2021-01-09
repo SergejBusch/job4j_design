e71263a769b614fb8e12d3cf6d470157b74b24eb
Mr. Anderson <h-blast@list.ru>
2021-01-09 22:50:41 +0100

    What is TDD? [#318477]

---
+++ chapter_004/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tdd;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+
+    @Ignore
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Ignore
+    @Test(expected = Exception.class)
+    public void whenBuyWithWrongDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2054, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = Exception.class)
+    public void whenBuyWithWrongPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 999, 1, date);
+    }
+}
