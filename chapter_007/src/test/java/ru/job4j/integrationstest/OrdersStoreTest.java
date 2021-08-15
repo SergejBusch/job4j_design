879c14e5263998e12409daf05be7fab931d5c1ea
SergejBusch <herimvsem@gmail.com>
2021-08-15 20:43:05 +0200

    integrationtests

---
+++ chapter_007/src/test/java/ru/job4j/integrationstest/OrdersStoreTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.integrationstest;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OrdersStoreTest {
+    private BasicDataSource pool = new BasicDataSource();
+
+    @Before
+    public void setUp() throws SQLException {
+        pool.setDriverClassName("org.hsqldb.jdbcDriver");
+        pool.setUrl("jdbc:hsqldb:mem:tests;sql.syntax_pgs=true");
+        pool.setUsername("sa");
+        pool.setPassword("");
+        pool.setMaxTotal(2);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream("./db/update_001.sql")))
+        ) {
+            br.lines().forEach(line -> builder.append(line).append(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pool.getConnection().prepareStatement(builder.toString()).executeUpdate();
+    }
+
+    @Test
+    public void whenSaveOrderAndFindAllOneRowWithDescription() {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertEquals(all.size(), 1);
+        assertEquals(all.get(0).getDescription(), "description1");
+        assertEquals(all.get(0).getId(), 1);
+
+        store.update(Order.of("name2", "description2"), 1);
+
+        all = (List<Order>) store.findByName("name2");
+
+        assertEquals(all.get(0).getDescription(), "description2");
+        assertEquals(all.get(0).getId(), 1);
+    }
+}
